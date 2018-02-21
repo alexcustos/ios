c2b4de49d698b1b00b81a820f831d96af6335823
Marino Faggiana <m.faggiana@twsweb.it>
2018-02-21 15:06:50 +0100

    start

---
+++ PickerFileProvider/FileProviderManager.swift
@@ -0,0 +1,16 @@
+//
+//  FileProviderManager.swift
+//  PickerFileProvider
+//
+//  Created by Marino Faggiana on 06/02/18.
+//  Copyright © 2018 TWS. All rights reserved.
+//
+
+import Foundation
+
+@available(iOSApplicationExtension 11.0, *)
+
+public class FileProviderManager: NSFileProviderManager {
+    
+}
+
